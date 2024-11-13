9ce4a016cd4d681312df4d50f6ee8a7adfa5fab1
seunwon <angelaseo2290@gmail.com>
2024-11-13 15:28:16 +0900

    end

--- lib/main.dart
+++ lib/main.dart
@@ -7,59 +7,136 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, primaryColor: Colors.blue),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Sign Up Page'),
-          ),
-          body: Column(
+        appBar: AppBar(
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset('lib/pictures/logo.png', width: 100, height: 100),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your username',
-                ),
-              ),
+              SizedBox(height: 60),
+              Text('Create your account'),
               SizedBox(height: 20),
               TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your password',
-                ),
-              ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF2D4739),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Color(0xFF2D4739),
+                    )),
+                  ),
+                  cursorColor: Color(0xFF2D4739)),
               SizedBox(height: 20),
               TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Verify the password',
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(top: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Add the code to sign up the user
-                  },
-                  child: const Text('Sign Up with Google'),
-                ),
-              ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your password',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF2D4739),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Color(0xFF2D4739),
+                    )),
+                  ),
+                  cursorColor: Color(0xFF2D4739)),
+              SizedBox(height: 20),
+              TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Verify your password',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF2D4739),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Color(0xFF2D4739),
+                    )),
+                  ),
+                  cursorColor: Color(0xFF2D4739)),
               SizedBox(height: 20),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF2D4739), // 배경색 설정
+                  foregroundColor: Colors.white, // 텍스트 색상 설정
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5), // 버튼 모서리 둥글게 설정
+                  ),
+                ),
                 onPressed: () {
                   // Add the code to sign up the user
                 },
                 child: const Text('Sign Up'),
               ),
+
+              SizedBox(height: 20),
+
+              //다른 방법으로 로그인
+              Text('Or sign up with...'),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, // 배경색 설정
+                      foregroundColor: Colors.black, // 텍스트 색상 설정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5), // 버튼 모서리 둥글게 설정
+                      ),
+                    ),
+                    onPressed: () {
+                      // Add the code to sign up with Google
+                    },
+                    child: const Text('Google'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, // 배경색 설정
+                      foregroundColor: Colors.black, // 텍스트 색상 설정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5), // 버튼 모서리 둥글게 설정
+                      ),
+                    ),
+                    onPressed: () {
+                      // Add the code to sign up with Facebook
+                    },
+                    child: const Text('Facebook'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, // 배경색 설정
+                      foregroundColor: Colors.black, // 텍스트 색상 설정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5), // 버튼 모서리 둥글게 설정
+                      ),
+                    ),
+                    onPressed: () {
+                      // Add the code to sign up with Apple
+                    },
+                    child: const Text('Apple'),
+                  ),
+                ],
+              ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
